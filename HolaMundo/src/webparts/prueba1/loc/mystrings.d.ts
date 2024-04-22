5b5c8f29633fe0d2515552e8b140abde92c495f0
Rubén <rubencabo@comasis.com>
2024-04-22 09:48:10 +0200

    SPFX5

--- HolaMundo/src/webparts/prueba1/loc/mystrings.d.ts
+++
@@ -1,7 +0,0 @@
-declare interface IPrueba1WebPartStrings {
-}
-
-declare module 'Prueba1WebPartStrings' {
-  const strings: IPrueba1WebPartStrings;
-  export = strings;
-}
--- prueba 3/src/webparts/prueba/components/IPruebaProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IPruebaProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
